6e9dd9efc602bd8b947021be0fb9458f8ecd8e17
madhuc25 <madhuc@maveric-systems.com>
2022-11-23 16:04:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const makeRequest = require('./utilities').makeRequest;
+
+const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:8100"
+  };
+
+app.use(cors(corsOptions));
+
+app.set('json spaces', 4);
+
+app.listen(3000, () => console.log("Server running at port " + 3000));
+
+app.post('/checkout', async (req, res) => {
+    try {
+        body = {
+            amount:145,
+            complete_payment_url:"http://example.com/complete",
+            country:"IN",
+            currency:"INR",
+            error_payment_url:"http://example.com/error",
+            merchant_reference_id:"950ae8c6-78",
+            cardholder_preferred_currency:true,
+            language:"en",
+            metadata:{
+                "merchant_defined":true
+            },
+            payment_method_type_categories:['bank_redirect','bank_transfer','card','cash','ewallet'],
+            payment_method_types_include:[],
+            expiration:"1671532380",
+        };
+        const result = await makeRequest('POST', '/v1/checkout', body);
+    
+        res.json(result);
+      } catch (error) {
+        res.json(error);
+      }
+
+})
+
+app.get('/payment', async (req, res) => {
+
+    try {
+        const body = {
+            amount: 230,
+            currency: 'MXN',
+            payment_method: {
+                type: 'mx_diestel_cash'
+            }
+        };
+        const result = await makeRequest('POST', '/v1/payments', body);
+        res.json(result);
+    } catch (error) {
+        res.json(error);
+    }
+
+})
+
+app.get('/country', async (req, res) => {
+
+    try {
+        const result = await makeRequest('GET', '/v1/payment_methods/country?country=in');
+    
+        res.json(result);
+      } catch (error) {
+        res.json(error);
+      }
+
+})
---
+++ utilities.js
@@ -0,0 +1,126 @@
+const https = require('https');
+const crypto = require('crypto');
+const accessKey = "30219A5F57C46AAF86ED";
+const secretKey = "b1144db21861cb0c0ef7097944c111b0dd1624644563d20f7a0ccde036839e621b200f7aceedb961";
+const log = false;
+
+async function makeRequest(method, urlPath, body) {
+
+    try {
+        httpMethod = method;
+        httpBaseURL = "sandboxapi.rapyd.net";
+        httpURLPath = urlPath;
+        salt = generateRandomString(8);
+        idempotency = new Date().getTime().toString();
+        timestamp = Math.round(new Date().getTime() / 1000).toString();
+        signature = sign(httpMethod, httpURLPath, salt, timestamp, body)
+        const options = {
+            hostname: httpBaseURL,
+            port: 443,
+            path: httpURLPath,
+            method: httpMethod,
+            headers: {
+                'Content-Type': 'application/json',
+                salt: salt,
+                timestamp: timestamp,
+                signature: signature,
+                access_key: accessKey,
+                idempotency: idempotency
+            }
+        }
+
+        return await httpRequest(options, body, log);
+    }
+    catch (error) {
+        console.error("Error generating request options");
+        throw error;
+    }
+}
+
+function sign(method, urlPath, salt, timestamp, body) {
+
+    try {
+        let bodyString = "";
+        if (body) {
+            bodyString = JSON.stringify(body);
+            bodyString = bodyString == "{}" ? "" : bodyString;
+        }
+
+        let toSign = method.toLowerCase() + urlPath + salt + timestamp + accessKey + secretKey + bodyString;
+        log && console.log(`toSign: ${toSign}`);
+
+        let hash = crypto.createHmac('sha256', secretKey);
+        hash.update(toSign);
+        const signature = Buffer.from(hash.digest("hex")).toString("base64")
+        log && console.log(`signature: ${signature}`);
+
+        return signature;
+    }
+    catch (error) {
+        console.error("Error generating signature");
+        throw error;
+    }
+}
+
+function generateRandomString(size) {
+    try {
+        return String(crypto.randomBytes(size).toString('hex'));
+    }
+    catch (error) {
+        console.error("Error generating salt");
+        throw error;
+    }
+}
+
+async function httpRequest(options, body) {
+
+    return new Promise((resolve, reject) => {
+
+        try {
+            
+            let bodyString = "";
+            if (body) {
+                bodyString = JSON.stringify(body);
+                bodyString = bodyString == "{}" ? "" : bodyString;
+            }
+
+            log && console.log(`httpRequest options: ${JSON.stringify(options)}`);
+            const req = https.request(options, (res) => {
+                let response = {
+                    statusCode: res.statusCode,
+                    headers: res.headers,
+                    body: ''
+                };
+
+                res.on('data', (data) => {
+                    response.body += data;
+                });
+
+                res.on('end', () => {
+
+                    response.body = response.body ? JSON.parse(response.body) : {}
+                    log && console.log(`httpRequest response: ${JSON.stringify(response)}`);
+
+                    if (response.statusCode !== 200) {
+                        return reject(response);
+                    }
+
+                    return resolve(response);
+                });
+            })
+            
+            req.on('error', (error) => {
+                return reject(error);
+            })
+            
+            req.write(bodyString)
+            req.end();
+        }
+        catch(err) {
+            return reject(err);
+        }
+    })
+
+}
+
+exports.makeRequest = makeRequest;
