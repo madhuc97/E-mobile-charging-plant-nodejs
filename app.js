65db31354663af02ca3daaa4144df4cb7cac640e
madhuc25 <madhuc@maveric-systems.com>
2022-12-05 11:51:43 +0530

    initial commit

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ const corsOptions = {
     optionsSuccessStatus: 200
   };
 
-//   app.all('*', function(req, res, next) {
-//     const origin = cors.origin.contains(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
-//     res.header("Access-Control-Allow-Origin", origin);
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+  app.all('*', function(req, res, next) {
+    const origin = cors.origin.contains(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(cors(corsOptions));
 
