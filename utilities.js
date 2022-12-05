c5f7e26db0742c13e786deacdd7e356f0c4b3675
madhuc25 <madhuc@maveric-systems.com>
2022-12-05 15:06:45 +0530

    initial commit

--- utilities.js
+++ utilities.js
@@ -23,7 +23,7 @@ async function makeRequest(method, urlPath, body) {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': 'http://localhost:8100',
+                'Access-Control-Allow-Origin': '*',
                 salt: salt,
                 timestamp: timestamp,
                 signature: signature,
